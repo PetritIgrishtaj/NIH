89abc42cd45c108958fcdb5c2e0643a3981146d8
PetritIgrishtaj <85747899+PetritIgrishtaj@users.noreply.github.com>
2021-06-16 15:24:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ transform = transforms.Compose([
     # transforms.Resize(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
+                         std=[0.229, 0.224, 0.225]),
+    transforms.RandomAffine(7)
 ])
 
 def main():
