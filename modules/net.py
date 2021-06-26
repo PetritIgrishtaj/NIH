1fe0f0dd957029c9a1347c6b9e316d4efbc4f2f6
PetritIgrishtaj <85747899+PetritIgrishtaj@users.noreply.github.com>
2021-06-26 17:30:13 +0200

    Update net.py

--- modules/net.py
+++ modules/net.py
@@ -8,8 +8,8 @@ def get_model(num_classes):
     model = models.vgg19(pretrained=True, progress=True)
 
     # change the last linear layer
-    #num_ftrs = model.fc.in_features
-    num_ftrs = model.classifier.in_features
-    #model.fc = nn.Linear(num_ftrs, num_classes)
-    model.classifier = nn.Linear(num_ftrs, num_classes)
+    num_ftrs = model.fc.in_features
+    #num_ftrs = model.classifier.in_features
+    model.fc = nn.Linear(num_ftrs, num_classes)
+    #model.classifier = nn.Linear(num_ftrs, num_classes)
     return model
