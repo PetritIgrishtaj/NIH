ad9acc4644c64bc2ec688e6542e6586456a544c2
PetritIgrishtaj <85747899+PetritIgrishtaj@users.noreply.github.com>
2021-06-26 16:20:34 +0200

    Update net.py

--- modules/net.py
+++ modules/net.py
@@ -5,7 +5,7 @@ from torchvision import datasets, models, transforms
 
 
 def get_model(num_classes):
-    model = models.resnet50(pretrained=True, progress=True)
+    model = models.EfficientNetB1(pretrained=True, progress=True)
 
     # change the last linear layer
     num_ftrs = model.fc.in_features
