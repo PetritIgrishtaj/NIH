f76bc88345f7640944cd71c7b286f9fba8b713c7
PetritIgrishtaj <85747899+PetritIgrishtaj@users.noreply.github.com>
2021-06-28 12:13:33 +0200

    Update net.py

--- modules/net.py
+++ modules/net.py
@@ -2,15 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, models, transforms
-from efficientnet_pytorch import EfficientNet
 
 def get_model(num_classes):
-    #model = models.resnet50(pretrained=True, progress=True)
-    model = EfficientNet.from_pretrained('efficientnet-b7', include_top=True, num_classes=num_classes)
+    model = models.resnet50(pretrained=True, progress=True)
+    #model = EfficientNet.from_pretrained('efficientnet-b7', include_top=True, num_classes=num_classes)
    
     # change the last linear layer
-    #num_ftrs = model.fc.in_features
+    num_ftrs = model.fc.in_features
     #num_ftrs = model.classifier.in_features
-    #model.fc = nn.Linear(num_ftrs, num_classes)
+    model.fc = nn.Linear(num_ftrs, num_classes)
     #model.classifier = nn.Linear(num_ftrs, num_classes)
     return model
