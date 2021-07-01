47d38bfff4cd226756cc416493177a3c21f48011
PetritIgrishtaj <85747899+PetritIgrishtaj@users.noreply.github.com>
2021-07-01 10:33:57 +0200

    Update net.py

--- modules/net.py
+++ modules/net.py
@@ -8,8 +8,8 @@ def get_model(num_classes):
     #model = EfficientNet.from_pretrained('efficientnet-b7', include_top=True, num_classes=num_classes)
    
     # change the last linear layer
-    num_ftrs = model.fc.in_features
-    #num_ftrs = model.classifier.in_features
-    model.fc = nn.Linear(num_ftrs, num_classes)
-    #model.classifier = nn.Linear(num_ftrs, num_classes)
+    #num_ftrs = model.fc.in_features
+    num_ftrs = model.classifier.in_features
+    #model.fc = nn.Linear(num_ftrs, num_classes)
+    model.classifier = nn.Linear(num_ftrs, num_classes)
     return model
