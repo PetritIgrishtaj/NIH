f05599d60f2b56d6c719de8bae0ed17e51c47857
PetritIgrishtaj <85747899+PetritIgrishtaj@users.noreply.github.com>
2021-07-02 11:49:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def main():
     parser.add_argument('--log-interval', type = int, default = 5, help = 'log every n batches')
     parser.add_argument('--save-interval', type = int, default = 5, help = 'save every n batches')
     parser.add_argument('--data-frac', type = float, default = 1, help = 'use only fraction of the data')
-    parser.add_argument('--folds', type=int, default=5, help='how many folds to produce')
-    parser.add_argument('--fold-id', type=int, default=0, help='Which fold id to use for test/val split')
+    #parser.add_argument('--folds', type=int, default=5, help='how many folds to produce')
+    #parser.add_argument('--fold-id', type=int, default=0, help='Which fold id to use for test/val split')
     parser.add_argument('--seed', type=int, default=0, help='Seed the random generator to get reproducability')
     args = parser.parse_args()
 
@@ -61,18 +61,18 @@ def main():
 
 
     data_wrapper = ChestXRayImages(root  = args.data_path,
-                                   folds = args.folds,
+                                   #folds = args.folds,
                                    frac  = args.data_frac,
                                    seed  = args.seed)
 
     data_train = ChestXRayImageDataset(
         args.data_path,
-        data_wrapper.data_train(args.fold_id),
+        data_wrapper.data_train#(args.fold_id),
         transform=transform
     )
     data_val = ChestXRayImageDataset(
         args.data_path,
-        data_wrapper.data_val(args.fold_id),
+        data_wrapper.data_val#(args.fold_id),
         transform=transform
     )
     data_test = ChestXRayImageDataset(
